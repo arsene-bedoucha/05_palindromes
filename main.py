bea3bdb737b8d258d16c7ebdc0a4fe9ad2352a3a
arsene-bedoucha <arsene.bedoucha@edu.esiee.fr>
2025-09-25 13:54:43 +0000

    palindromes arsene-bedoucha

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
-#### Fonction secondaire
+""" Ce module renvoie si des mots dans une liste sont des palindromes ou non"""
 
+#### Fonction secondaire ###
 
 def ispalindrome(p):
+    """La fonction regarde si un mot est un palindrome ou pas"""
 
-    # votre code ici
-    
-    return False
+    a = p.lower()
+    a = a.replace("é", "e")
+    a = a.replace("è", "e")
+    a = a.replace("à", "a")
+    a = a.replace("ê", "e")
+    a = a.replace("ë", "e")
+    a = a.replace("ô", "o")
+    a = a.replace(",", "")
+    a = a.replace("'", "")
+    a = a.replace(" ", "")
+    a = a.replace("-", "")
+    a = a.replace(".", "")
+    a = a.replace("?", "")
+    a = a.replace("!", "")
+    a = a.replace(":", "")
+    a = a.replace("ç", "c")
+    i = a[::-1]
+    t = 0
+    while t < len(a)-1:
+        if a[t] != i[t] :
+            t = t+1
+            print('False')
+            break
+    return True
 
-#### Fonction principale
-
+#### Fonction principale ###
 
 def main():
-
-    # vos appels à la fonction secondaire ici
+    """La fonction contient la liste des mots à tester"""
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
 
 if __name__ == "__main__":
     main()
